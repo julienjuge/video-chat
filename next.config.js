ba164a70f36d39477af17bdba251e7182f4c830e
Julien JUGE <julien.juge@live.fr>
2020-04-01 09:59:56 +0200

    test

--- next.config.js
+++ next.config.js
@@ -3,7 +3,8 @@ module.exports = {
     twilioAccountSid: "AC4d2c8c50fd3c9b384603ad2ef84b12cb",
     twilioApiKey: "SK3223f534530f31b674d8ba7924822620",
     twilioApiSecret: "OlF96IBLN1ZTg6MJIPi3K4xRAvycR7XS",
-    reactAppSignalingServer: "https://video-chat.julienjuge.now.sh",
+    reactAppSignalingServer:
+      "http://app-94785831-51b8-4834-9905-54e1ebcfe828.cleverapps.io/",
     reactAppStunServers: "stun:stun4.l.google.com:19302",
     reactAppTurnServers: "turn:numb.viagenie.ca",
     reactAppTurnUsername: "julien.juge@live.fr",
